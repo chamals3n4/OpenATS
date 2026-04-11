e2428c3b5946d9ed9b12050b4597b3bdb1af6cf3
Chamal Senarathna <chamals004@gmail.com>
2026-04-11 18:24:22 +0530

    perfect

--- api/src/app.ts
+++ api/src/app.ts
@@ -60,7 +60,7 @@ app.use(
   ),
 );
 app.get("/health", (req, res) => {
-  res.status(200).json({ status: "working perfectly" });
+  res.status(200).json({ status: "perfect" });
 });
 
 app.use("/public", publicRouter);

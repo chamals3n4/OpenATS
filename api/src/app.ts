256d08d45c5a7d1302d11bc632f19773db643d80
Chamal Senarathna <chamals004@gmail.com>
2026-04-23 00:52:43 +0530

    dsd

--- api/src/app.ts
+++ api/src/app.ts
@@ -63,7 +63,7 @@ app.use(
   ),
 );
 app.get("/health", (req, res) => {
-  res.status(200).json({ status: "good" });
+  res.status(200).json({ status: "working" });
 });
 
 app.use("/public", publicRouter);
